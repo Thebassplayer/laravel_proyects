7c9922551ae77fe12d3f4f7b26ed26e09f593166
Roy Lopez <ryhsu2@gmail.com>
2024-09-16 18:22:50 +0200

    Refactor UserController to add registration, login, and logout functionality

--- main_app/app/Http/Controllers/UserController.php
+++ main_app/app/Http/Controllers/UserController.php
@@ -17,8 +17,11 @@ class UserController extends Controller
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
-        return 'Hello from register function';
+        $user = User::create($incomingFields);
+
+        auth()->login($user);
+
+        return redirect('/')->with('success', 'Thank you for creating an account');
     }
 
     public function login(Request $request){
@@ -29,10 +32,15 @@ class UserController extends Controller
 
         if(auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])){
             $request->session()->regenerate();
-            return 'Congrats!!!';
+            return redirect('/')->with('success', 'You have successfully logged in.');
         } else {
-            return 'Sorry!!!';
+            return redirect('/')->with('failure', 'Invalid Credentials');
         }
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect('/')->with('success', 'You are now logged out.');
     }
 
     public function showCorrectHomepage(){
